e8475aaf35e86c8c3d9c1638a8547c95bd7d4ebc
natalieosita <ositanatalie@gmail.com>
2025-03-17 22:45:24 +0300

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,23 @@
+#Key in the numerical values
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /): ")
+
+#calculations
+if operation == "+":
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == "-":
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == "*":
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Mathematical error")
+else:
+    print("Invalid operation. Please enter +, -, *, or /.")
